7014fc552bc976244ac7ae92b9eeecb600b02898
okamura nana <nana19820227@gmail.com>
2025-12-12 22:53:48 +0900

    fix build.sh (use python3 -m pip)

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate
