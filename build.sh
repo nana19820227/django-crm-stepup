731461ee85a0c29ee9620797c32337d5b004c761
okamura nana <nana19820227@gmail.com>
2025-12-12 22:35:53 +0900

    finalize build.sh for Render

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "=== Upgrade pip ==="
-pip install --upgrade pip
-
-echo "=== Install requirements ==="
 pip install -r requirements.txt
 
-echo "=== Collect static files ==="
-python manage.py collectstatic --noinput
-
-echo "=== Apply database migrations ==="
-python manage.py migrate
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
+python3 manage.py create_superuser_if_not_exists
